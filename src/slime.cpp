13f50fd910c394fb27b01f875ec24cd78ecd50ef
Elijah J Molina <51384765+ImKirihara@users.noreply.github.com>
2024-11-14 01:32:39 -0500

    Inital Slime Classes

---
+++ src/slime.cpp
@@ -0,0 +1,22 @@
+#include "slime.h"
+#include <godot_cpp/classes/resource_loader.hpp>
+
+
+using namespace godot;
+
+void Slime::_bind_methods() {}
+
+Slime::Slime() {
+	
+}
+
+Slime::~Slime() {}
+
+void Slime::_ready (){
+	//Initalize Slime to be a Sphere Mesh Based Maybe (Softbody)
+	
+}
+
+void Slime::_process(double delta){
+
+}
---
+++ src/slime.h
@@ -0,0 +1,49 @@
+#ifndef SLIME_H
+#define SLIME_H
+
+#include <godot_cpp/classes/node3d.hpp>
+#include <godot_cpp/classes/sphere_mesh.hpp>
+
+#include <godot_cpp/variant/utility_functions.hpp>
+
+#include <godot_cpp/classes/mesh_instance3d.hpp>
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/engine.hpp>
+#include "enemy.h"
+
+enum State {
+    IDLE, //Will Stand Still But if a Player enters its range it will Chase
+    WANDER, //Random Walks around if there is a Player in Range it will Chase
+    CHASE //Follow the Last Seen Position in a Radius of a Player
+};
+
+namespace godot {
+
+class QuatCamera;
+class Slime : public  Enemy{
+	GDCLASS(Slime, Enemy);
+
+protected:
+	static void _bind_methods();
+	
+
+
+private:
+	bool checkForPlayer();
+	void chasePlayer();
+	
+	
+
+public:
+	Slime();
+	~Slime();
+
+    void _ready() override;
+	void _process(double delta) override;
+
+};
+
+}
+
+#endif
